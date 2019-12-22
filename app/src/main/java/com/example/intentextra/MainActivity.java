942f6fcdeac250db27d0414623534499896fc341
吴翱翔 <pylint@yandex.com>
2019-12-22 10:53:32 +0800

    解决了Manifest.xml两个警告——Android6.0以下的SDK安装时会自动备份数据 以及 Not Index by Google Search

--- app/src/main/java/com/example/intentextra/MainActivity.java
+++ app/src/main/java/com/example/intentextra/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/* 发现SDK23(安卓6.0)依然有各种遗留问题
+ * 如AndroidMainfest.xml: 会有警告App data will Automatically backup and restored on install
+ * 解决方案：Applicaton标签新增属性 android:allowBackup="true"
+ * 下次我创建项目一定要选9.0的SDK(模拟器是10.0的，我手机9.0的)
+ *  */
 public class MainActivity extends AppCompatActivity {
 
   @Override
